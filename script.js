9e74dc6637263f698cb6b3be67dd0753cf9ae6aa
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-06-15 22:33:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstChar(text) {
   // your code here
+	let t=text.trim();
+
+	if(t==""){
+		return '';
+	}
+	else{
+		return t.charAt(0);
+	}
 }
-
 // Do not change the code below
-
 const text = prompt("Enter text:");
 alert(firstChar(text));
